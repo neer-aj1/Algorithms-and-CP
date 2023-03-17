284f122f8eb429f63bb498db27f742bf2ccce060
Neeraj Kumar <neerajkumar15439@gmail.com>
2023-03-17 10:47:35 +0530

    Added Kaden Algorithm

--- practice.cpp
+++ practice.cpp
@@ -2,33 +2,18 @@
 using namespace std;
 typedef long long ll;
 
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while(t--){
-    int n;
-    int m;
-    int flag, count=0;
-    cin >> n >> m;
-    int arr[n][m];
-    for(int i=0; i<n; i++){
-      for(int j=0; j<m; j++){
-        cin >> arr[i][j];
-      }
+class Solution {
+    //Kaden's Algorithm
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum=0;
+        int maxSum=0;
+        for(auto it: nums){
+            currentSum+=it;
+            if(currentSum > maxSum) maxSum=currentSum;
+            if(currentSum < 0) currentSum=0;
+        }
+        return maxSum;
     }
-    for(int i=0; i<n; i++){
-      flag=0;
-      for(int j=0; j<m; j++){
-        if(arr[i][j]==1) flag=1;
-      }
-      if(flag==1) count++;
-    }
-    if((n-count)%2==0) cout << "Vivek" << endl;
-    else cout << "Ashish" << endl;
-  }
-  return 0;
-}
+};
+//kaden algorithm is used to find the subarray with maximum sum
